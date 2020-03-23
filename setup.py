194081dc830fcddb388adcf02d26ae7830b6ce6e
Alexander Böhn <fish2000@gmail.com>
2020-03-23 19:18:35 -0400

    Bumping the bumpversion versioning version

--- setup.py
+++ setup.py
@@ -1,11 +1,28 @@
 #!/usr/bin/env python
 from setuptools import setup
-import sys
+import sys, os
+
+PROJECT_NAME = 'pythonpy'
+
+# PROJECT DIRECTORY
+CWD = os.path.dirname(__file__)
+BASE_PATH = os.path.join(
+            os.path.abspath(CWD), PROJECT_NAME)
 
 py_entry = 'ppy%s = pythonpy.__main__:main'
 pycompleter_entry = 'ppycompleter%s = pythonpy.pycompleter:main'
 endings = ('', sys.version[:1], sys.version[:3])
 entry_points_scripts = []
+
+# PROJECT VERSION & METADATA
+__version__ = "<undefined>"
+try:
+    exec(compile(
+        open(os.path.join(BASE_PATH,
+            '__version__.py')).read(),
+            '__version__.py', 'exec'))
+except:
+    __version__ = '0.5.1'
 
 for e in endings:
     entry_points_scripts.append(py_entry % e)
@@ -13,7 +30,7 @@ for e in endings:
 
 setup(
     name='pythonpy-fork',
-    version='__version__ = '0.5.1'',
+    version=__version__,
     description='python -c, with tab completion and shorthand; fish2k fork',
     license='MIT',
     url='https://github.com/fish2000/pythonpy-fork',
